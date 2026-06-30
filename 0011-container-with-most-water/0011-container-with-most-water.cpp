2e9866f6f5eecb94852ea9cd5997b7ea3119a94e
Satyam Sachan <satyamsachan04@gmail.com>
2026-06-30 12:48:38 +0530

    Time: 0 ms (100%), Space: 63.1 MB (10.74%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,19 +4,24 @@ public:
 
         int l=0;
         int r=height.size()-1;
-
         int ans=0;
-
         while(l<r){
-
-            ans=max(ans,min(height[l],height[r])*(r-l));
-
-            if(height[l]<height[r])
+            int h;
+            if(height[l]<height[r]){
+                h=height[l];
+            }
+            else{
+                h=height[r];
+            }
+            int w=r-l;
+            ans=max(ans,h*w);
+            if(height[l]<height[r]){
                 l++;
-            else
+            }
+            else{
                 r--;
+            }
         }
-
         return ans;
     }
 };
