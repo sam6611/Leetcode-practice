394e53eebf413df058e4f4d6e252321c36875976
Satyam Sachan <satyamsachan04@gmail.com>
2026-07-25 07:08:27 +0530

    Time: 0 ms (100%), Space: 8.9 MB (81.57%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                if(st.empty()){
+                    st.push(s[i]);
+                }
+                else{
+                    ans+=s[i];
+                    st.push(s[i]);
+                }
+            }
+            else{
+                    st.pop();
+                if(!st.empty()){
+                    ans+=s[i];
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
