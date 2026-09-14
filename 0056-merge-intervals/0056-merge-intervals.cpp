6ad3720bc31d3e9d9347c52a3d11d66692c626e2
Satyam Sachan <satyamsachan04@gmail.com>
2026-09-14 15:44:14 +0530

    Time: 3 ms (89.29%), Space: 24.1 MB (22.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end(),
-             [](vector<int>& a, vector<int>& b) { return a[0] < b[0]; });
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end(), [](vector<int>&a , vector<int>&b) {return a[0]<b[0];});
+        int start=intervals[0][0];
+        int end=intervals[0][1];
         vector<vector<int>> store;
-
-        for (int i = 1; i < n; i++) {
-            int first = intervals[i][0];
-            int second = intervals[i][1];
-
-            if (first > end) {
-                store.push_back({start, end});
-                start = first;
-                end = second;
-            } else {
-                end = max(end, second);
+        for(int i=1;i<n;i++){
+            int first=intervals[i][0];
+            int sec=intervals[i][1];
+            if(first<=end){
+                end=max(sec,end);
+            }
+            else{
+                store.push_back({start,end});
+                start=first;
+                end=sec;
             }
         }
-        
         store.push_back({start, end});
-        
-        return store;
+            return store;
     }
 };
